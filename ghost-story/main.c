150268f1628bd6e07236514b1506e4abab029e5d
Michel Cuypers <mcu@tectumgroup.be>
2020-05-04 14:11:30 +0000

    v0.2

--- ghost-story/main.c
+++ ghost-story/main.c
@@ -6,10 +6,13 @@
 void command();
 void startUp();
 int input();
-char * readline();
+char * readLine();
 char * fWord();
 void use();
-
+void search();
+void open();
+void go();
+void map();
 
 // INIT
 int answer;
@@ -23,56 +26,47 @@ int main()
     // TITLE SCREEN + INTRODUCTION
     startUp();
 
+
     // FIRST QUESTION
-    printf("[1] look around\t [2] go north\t [3] go south\n");
+    printf("[1] look around\t [2] go north\t [3] go south\n\n");
     do {
         input(&answer);
         switch(answer) 
         {
-            case 1: printf("\nIt's pouring, maybe it's better if we go somewhere to hide.\n\n"); break;
-            case 2: printf("\nYou go towards the mansion and stand in front of the doors.\nThere's a sign on the door that says: \"Begone humans! Leave the dead in peace!\"\n\n"); break;
-            case 3: printf("\nIt's too dark going south, it's freezing outside.\n\n"); break;
+            case 1: printf("It's pouring, maybe it's better if we go somewhere to hide.\n\n"); break;
+            case 2: printf("You go towards the mansion.\nThere's a sign on the door that says: \"Begone humans! Leave the dead in peace!\"\n\n"); break;
+            case 3: printf("It's too dark going south, it's freezing outside.\n\n"); break;
             default: printf("Answer %i is not an option.\n\n", answer); break;
         }
     } while (answer != 2);
+
     
     // SECOND QUESTION
-    printf("What do you do?\n\n");
-    printf("[1] Go inside\t [2] Knock on the door\t [3] Kick a flower pot\t\n");
+    printf("[1] Go inside\t [2] Knock on the door\t [3] Kick a flower pot\n\n");
 
     do {
         command();
         scanf("%i", &answer);
         switch(answer) 
         {
-            case 1: printf("\nIt's locked. You need to find a key.\n\n"); break;
-            case 2: printf("\nNobody's home, it seems abandonded too.\n\n"); break;
-            case 3: printf("\nWhilst kicking the flower pot you noticed underneath it you found a key.");
-                    printf("\nYou open the doors and go inside.\n\nIt's very dark inside.\n\n");
+            case 1: printf("It's locked. You need to find a key.\n\n"); break;
+            case 2: printf("Nobody's home, it seems abandonded too.\n\n"); break;
+            case 3: printf("You found the key under the flower pot!");
+                    printf("\nYou open the door, it's very dark inside..\n\n");
                     break;
             default: printf("Answer %i is not an option.\n\n", answer); break;
         }
     } while (answer != 3);
 
+
     // THIRD QUESTION
-    getchar(); // REMOVES NEWLINE FROM BUFFER
     printf("We could use some light..\n");
     printf("\n[!] To use a lighter, type 'USE' and hit enter. Then type 'LIGHTER'.\n\n");
+    getchar(); // REMOVES NEWLINE
 
-    // USE
-    while(1) {
-        command();
-        read = fWord();
-
-        if ((strcasecmp(read, "use") == 0)) 
-            break;
-        else
-            printf("I don't know the word %s.\n\n", read);
-    }
-    
-    // LIGHTER
-    while(1) {
-        use();
+    use(); // LOOKS AT USE FIRST
+    while(1) { // LIGHTER
+        
         read = fWord();
         
         if ((strcasecmp(read, "lighter") == 0)) 
@@ -81,10 +75,77 @@ int main()
             printf("I don't know the word %s.\n\n", read);
     }
 
+
     // FOURTH QUESTION
-    printf("\nThe room lightened up.\nWow seems like nobody has been here in years!\n");
-    printf("\nWhat's that?! You hear a sound coming out of the corridor closet.\n");
+    printf("The room lightened up a little, all windows are boarded.\n");
+    printf("What do you want to do next?\n\n");
+    printf("[1] use toilet     [2] turn on some lights     [3] search for candles\n\n");
+
+    do {
+        command();
+        scanf("%i", &answer);
+        switch(answer) 
+        {
+            case 1: printf("Aaah that felt good!\n\n"); break;
+            case 2: printf("Nothing happened.. I guess the power isn't working.\n\n"); break;
+            case 3: printf("You found a candle, just in time. Your lighter was almost empty.\n"); break;
+            default: printf("Answer %i is not an option.\n", answer); break;
+        }
+    } while (answer != 3);
+            printf("While looking for candles you also found a map which seems to be like the floorplan.\n");
+            
+    // MAP     
+    printf("\n[!] \"Use\" map where you want to go to:\n");
+    getchar();
+    use(); // LOOKS AT USE FIRST
+    // MAP
+    while (1)
+    {
+        read = fWord();
+        if(strcasecmp(read, "map") == 0)
+        {
+            map();
+            break;
+        }
+        else
+            printf("i don't know the word %s", read);        
+    }
+
+    printf("MAP:\n");
+    printf("Choose where you want to go to:\n");
+    printf("[1] KITCHEN     [2] DINING ROOM     [3] HAL     [4] LIVING ROOM     [5] UPSTAIRS\n\n");
+    do
+    {
+        command();
+        scanf("%i", &answer);
+        switch(answer)
+        {
+            case 1: 
+                printf("KITCHEN\n\n");
+                break;
+            case 2: 
+                printf("DINING ROOM\n\n");
+                break;
+            case 3: 
+                printf("HALL\n\n");
+                break;
+            case 4: 
+                printf("LIVING ROOM\n\n");
+                break;
+            case 5:
+                printf("UPSTAIRS\n");
+                printf("We haven't discovered all locations below yet.\n\n");
+            default: printf("Answer %i is not an option.\n\n", answer); break;
+        }
+    } while (answer < 1 ||answer > 4);
+        printf("Succes!\n");
+    
 }
+
+
+
+
+// FUNCTIONS
 
 // -->
 void command() {
@@ -92,8 +153,19 @@ void command() {
 }
 
 // USE
-void use() {
-    printf("USE:  ");
+void use() 
+{
+    while(1) {
+        command();
+        read = fWord();
+        if ((strcasecmp(read, "use") == 0)) 
+        {
+            printf("USE:  ");
+            break;
+        }
+        else
+            printf("I don't know the word %s.\n\n", read);
+    }
 }
 
 // INPUT
@@ -105,14 +177,57 @@ int input(int *answer)
     return *answer;
 }
 
+// SEARCH FUNCTION
+void search() 
+{
+    getchar(); // REMOVES ANY NEWLINE FROM BUFFER
+    while(1) 
+    {
+        command();
+        read = fWord();
+        if ((strcasecmp(read, "search") == 0)) 
+            break;
+        else
+            printf("I don't know the word %s.\n\n", read);
+    }
+}
+
+// OPEN FUNCTION
+void open() 
+{
+    while(1) 
+    {
+        command();
+        read = fWord();
+        if ((strcasecmp(read, "open") == 0)) 
+            break;
+        else
+            printf("I don't know the word %s.\n\n", read);
+    }
+}
+
+// OPEN FUNCTION
+void go() 
+{
+    while(1) 
+    {
+        command();
+        read = fWord();
+        if ((strcasecmp(read, "go") == 0)) 
+            break;
+        else
+            printf("I don't know the word %s.\n\n", read);
+    }
+}
+
 // READLINE
-char * readline() 
+char * readLine() 
 {
     char buffer[20];
     char * input = buffer;
-    int size = 32;
+    size_t size = 32;
 
-    fgets(input, size, stdin);
+    getline(&input, &size, stdin);
     input[strcspn(input, "\r\n")] = 0; // REMOVES NEWLINE FROM GETLINE
     
     return input;
@@ -123,13 +238,31 @@ char * fWord()
 {
     char buffer[20];
     char * input = buffer;
-    int size = 32;
+    size_t size = 32;
 
-    fgets(input, size, stdin);
+    getline(&input, &size, stdin);
     input = strtok(input, " ");
     input[strcspn(input, "\r\n")] = 0; // REMOVES NEWLINE FROM GETLINE
     
     return input;
+}
+
+void map()
+{
+        puts(
+        "\t\t\tWHIPSTAFF MANOR\n\n"
+        "   ______________________________________\n"
+        "  /            |        | UP |           \\ \n"
+        " |             | TOILET |----|            |\n"
+        " |   KITCHEN   |___ ____|----|            |\n"
+        " |                      |----|            |\n"
+        " |______  _____|        |                 |\n"
+        " |             |        |                 |\n"
+        " |    DINING   |              LIVING      |\n"
+        " |     ROOM             |      ROOM       |\n"
+        " |             |  HALL  |                 |\n"
+        " |_____________|________|_________________|\n"
+        );
 }
 
 // TITLE SCREEN & INTRODUCTION
@@ -138,11 +271,11 @@ void startUp()
     // TITLE SCREEN
     printf("\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("\t\t\t\tGHOST STORY v0.1\n");
+    printf("\t\t\tG H O S T    S T O R Y\n");
     printf("\n");
-    printf("\t\t\t    A TEXT-ADVENTURE GAME    \n");
+    printf("\t\t\tA TEXT-ADVENTURE GAME    \n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n");
 
     // INTRODUCTION
-    printf("\nIt's a cold day, mid december and you find yourself wandering outside.\n\nIn the distance you see a mansion.\n\n");
+    printf("\nIt's a cold day, mid december and you find yourself wandering outside. In the distance you see a mansion.\n\n");
 }
